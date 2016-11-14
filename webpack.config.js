ddae7c3b159782f327c8d95f8c1b429dcf69850c
Robert Smol <robert.smol@stereoit.com>
2016-11-14 21:04:59 +0100

    Added SPA for openshift

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var {resolve} = require("path")
   , HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const webpack = require('webpack');
+
 module.exports = {
   context: resolve(__dirname, 'src'),
   entry: {
@@ -11,7 +13,14 @@ module.exports = {
     // publicPath: "/assets/",
     filename: "bundle.[chunkhash].js"
   },
-  plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
+  plugins: [
+    new HtmlWebpackPlugin({ template: './index.html' }),
+    new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  })
+  ],
   module: {
      loaders: [
        { test: /\.json$/, loader: 'json' },
